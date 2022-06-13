79c31ef308d043f0ba977256cb41d2559e22a558
truong.hua <truong.hua@youthdev.net>
2022-06-13 23:22:54 +0700

    Keep testing connectivity more frequently by default to prevent network issue

--- bmemcached/protocol.py
+++ bmemcached/protocol.py
@@ -94,7 +94,7 @@ class Protocol(threading.local):
 
     MAXIMUM_EXPIRE_TIME = 0xfffffffe
 
-    CONNECTIVITY_TEST_EVERY_SECONDS = 5
+    CONNECTIVITY_TEST_EVERY_SECONDS = 0.1
 
     last_connectivity_test = 0
 
